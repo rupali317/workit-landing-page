8ac9a13c1720f9979db2d90231ca079a32d65cb7
PriyankaRC16 <rupali317@gmail.com>
2024-05-10 09:59:26 +0800

    Defining the stylings

--- src/styles/Global.style.jsx
+++ src/styles/Global.style.jsx
@@ -35,8 +35,8 @@ export const BaseStyles = createGlobalStyle`
         --line-height-xl-1: 5rem; /* 80px */
 
         /* Letter spacings */
-        --letter-negative-spacing-1: -0.01rem;/* -0.16px */
-        --letter-negative-spacing-2: -0.01125rem;/* -0.18px */
+        --letter-negative-spacing-1: -0.01rem; /* -0.16px */
+        --letter-negative-spacing-2: -0.01125rem; /* -0.18px */
 
         /* Font weights */
         --font-weight-regular: 400;
@@ -44,11 +44,51 @@ export const BaseStyles = createGlobalStyle`
         --font-weight-bold: 700;
 
         /* Spacing */
-        /* 5px, 11px, 12px, 16px, 16.3px, 23px, 23.5px, 24px, 28px, 31px, 32px, 40px, 58px, 64px, 71.71px, 74px, 100.29px, 156.71px */
+        --space-none: 0rem;
+        --space-xs-1: 0.3125rem; /* 5px */
+        --space-xs-2: 0.5rem; /* 8px */
+        --space-xs-3: 0.6875rem; /* 11px */
+        --space-xs-4: 0.75rem; /* 12px */
+        --space-xs-5: 1rem; /* 16px */
+        --space-xs-6: 1.01875rem; /* 16.3px */
+        --space-s-1: 1.4375rem; /* 23px */
+        --space-s-2: 1.46875rem; /* 23.5px */
+        --space-s-3: 1.5rem; /* 24px */
+        --space-s-4: 1.625rem; /* 26px */
+        --space-s-5: 1.75rem; /* 28px */
+        --space-s-6: 1.9375rem; /* 31px */
+        --space-base-1: 2rem; /* 32px */
+        --space-base-2: 2.3125rem; /* 37px */
+        --space-base-3: 2.4375rem; /* 39px */
+        --space-base-4: 2.5rem; /* 40px */
+        --space-base-5: 3rem; /* 48px */
+        --space-base-6: 3.4375rem; /* 55px */
+        --space-base-7: 3.5rem; /* 56px */
+        --space-m-1: 3.625rem; /* 58px */
+        --space-m-2: 4rem; /* 64px */
+        --space-m-3: 4.481875rem; /* 71.71px */
+        --space-m-4: 4.625rem; /* 74px */
+        --space-m-5: 5rem; /* 80px */
+        --space-m-6: 6.266875rem; /* 100.27px */
+        --space-m-7: 6.268125rem; /* 100.29px */
+        --space-l-1: 6.483125rem; /* 103.73px */
+        --space-l-2: 6.875rem; /* 110px */
+        --space-l-3: 7.61125rem; /* 121.78px */
+        --space-l-4: 9.705625rem; /* 155.29px */
+        --space-l-5: 9.794375rem; /* 156.71px */
+        --space-l-6: 10.0625rem; /* 161px */
+        --space-l-7: 10.3125rem; /* 165px */
+        --space-xl-1: 10.334375rem; /* 165.35px */
+        --space-xl-2: 10.6875rem; /* 171px */
+        --space-xl-3: 12.7025rem; /* 203.24px */
+        --space-xl-4: 12.735rem; /* 203.76px */
+        --space-xl-5: 15.9375rem; /* 255px */
+        --space-xl-6: 20.375rem; /* 326px */
 
         /* Border width */
         --border-width-s: 0.0625rem; /* 1px */
-        --border-width-m: 0.1875rem; /* 3px */
+        --border-width-m: 0.125rem; /* 2px */
+        --border-width-l: 0.1875rem; /* 3px */
 
         /* Border radius */
         --border-radius-none: 0rem;
