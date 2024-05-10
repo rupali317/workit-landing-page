f0ca6227eee045eb496c1ecd70350b9fadb7cf8d
PriyankaRC16 <rupali317@gmail.com>
2024-05-10 23:22:25 +0800

    Added reset styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { AccessInvitation } from "./components/AccessInvitation";
 import { Introduction } from "./components/Introduction";
 import { SellingPoint } from "./components/SellingPoint";
+import * as GlobalStyles from "./styles/Global.style";
 
 function App() {
   return (
     <>
+      <GlobalStyles.BaseStyles />
+      <GlobalStyles.ResetStyles />
+      <GlobalStyles.DefaultStyles />
       <Introduction />
       <SellingPoint />
       <AccessInvitation />
--- src/styles/Global.style.jsx
+++ src/styles/Global.style.jsx
@@ -120,6 +120,81 @@ export const BaseStyles = createGlobalStyle`
     }
 `;
 
-export const ResetStyles = createGlobalStyle``;
+export const ResetStyles = createGlobalStyle`
+    /* Box sizing rules */
+    *,
+    *::before,
+    *::after {
+        box-sizing: border-box;
+    }
+
+    * {
+        margin: 0;
+    }
+
+    /* Prevent font size inflation */
+    html {
+        -moz-text-size-adjust: none;
+        -webkit-text-size-adjust: none;
+        text-size-adjust: none;
+    }
+
+    /* Remove list styles on ul, ol elements with a list role, which suggests default styling will be removed */
+    ul[role='list'],
+    ol[role='list'] {
+        list-style: none;
+    }
+
+    /* Remove default margin in favour of better control in authored CSS */
+    body,
+    h1,
+    h2,
+    p {
+        margin-block-end: 0;
+    }
+
+    /* Set core body defaults */
+    body {
+        min-height: 100vh;
+        line-height: 1.5;
+        -webkit-font-smoothing: antialiased;
+    }
+
+    /* Set shorter line heights on headings and interactive elements */
+    h1,
+    h2,
+    button {
+        line-height: 1.1;
+    }
+
+    /* Balance text wrapping on headings */
+    h1,
+    h2 {
+        text-wrap: balance;
+    }
+
+    /* Avoid text overflows */
+    h1,
+    h2,
+    p {
+        overflow-wrap: break-word;
+    }
+
+    /* Remove built-in form typography styles */
+    button {
+        font: inherit;
+    }
+
+    /* A elements that don't have a class get default styles */
+    a:not([class]) {
+        text-decoration-skip-ink: auto;
+    }
+
+    /* Make images easier to work with */
+    img {
+        max-width: 100%;
+        display: block;
+    }
+`;
 
 export const DefaultStyles = createGlobalStyle``;
