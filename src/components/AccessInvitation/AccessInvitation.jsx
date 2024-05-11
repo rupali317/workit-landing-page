c7b64bc005d4b9e856eebc8266dc2c3722d19bfc
PriyankaRC16 <rupali317@gmail.com>
2024-05-11 18:40:49 +0800

    Trying an approach where the width and height attribute changes based on state variables

--- src/components/AccessInvitation/AccessInvitation.jsx
+++ src/components/AccessInvitation/AccessInvitation.jsx
@@ -11,12 +11,53 @@ export const AccessInvitation = () => {
     );
   });
 
+  const DESKTOP_WIDTH_INTEGER = 80;
+  const BREAKPOINT =
+    DESKTOP_WIDTH_INTEGER *
+    parseFloat(
+      window.getComputedStyle(document.querySelector("html")).fontSize
+    );
+  const [windowInnerWidth, setWindowInnerWidth] = React.useState(
+    window.innerWidth
+  );
+  const [breakpoint, setBreakpoint] = React.useState(BREAKPOINT);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", () => {
+      // console.log("resized");
+      // console.log(window.innerWidth);
+      // console.log(ROOT_FONT_SIZE);
+      setWindowInnerWidth(window.innerWidth);
+      setBreakpoint(BREAKPOINT);
+    });
+  }, [windowInnerWidth, BREAKPOINT]);
+
+  // React.useEffect(() => {
+  //   const targetElement = document.querySelector("html");
+  //   const observer = new MutationObserver(() => {
+  //     if (DESKTOP_WIDTH_INTEGER * parseFloat(window.getComputedStyle(targetElement).fontSize) !== ROOT_FONT_SIZE) {
+  //       setWindowDesktopWidth(ROOT_FONT_SIZE);
+  //     }
+  //   });
+  //   observer.observe(targetElement, {attributes:true, attributeFilter:["style"]});
+  // }, [ROOT_FONT_SIZE]);
+
   return (
     <section>
       <img
         src="/assets/images/image-founder.webp"
         alt="Workit's founder"
         title="Workit's founder"
+        width={
+          windowInnerWidth > breakpoint 
+            ? "477"
+            : "281"
+        }
+        height={
+          windowInnerWidth > breakpoint 
+            ? "477"
+            : "281"
+        }
       />
       <section>
         <h2>Be the first to test</h2>
